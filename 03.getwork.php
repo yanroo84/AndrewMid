28fb4bb8ab304d8b550984fa5d4d62cbbac517df
AndrewWang <yanroo8444@gmail.com>
2015-11-08 20:45:14 +0800

    get work W/ comment

--- 03.getwork.php
+++ 03.getwork.php
@@ -9,19 +9,21 @@
 </head>
 
 <body>
-
+<!--接工作-->
 <p>Get work</p>
 <hr />
 <?php
 $id=(int)$_GET['id'];
 
 if ($id>0) {
+    //更改案子狀態為1(已接案)，並記錄誰接了工作
 	$sql = "update work set status=1,whotake='" . $_SESSION['uID'] . "' where id=$id;";
 	mysqli_query($conn,$sql) or die("MySQL insert message error"); //執行SQL
 	echo "work taked.";
 } else {
 	echo "empty id, cannot cancel.";
 }
+//雇主承接人都可以接案，因此判斷idtype，並導回其list頁面
 if($_SESSION['idtype'] == 0)
     header("Location: 02.list1.php");
 if($_SESSION['idtype'] == 1)
